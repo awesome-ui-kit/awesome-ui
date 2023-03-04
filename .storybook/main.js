398186ed7f283e9d17dc9acc8b71267fcfa248cb
Karina <karina_karapetyan@mail.ru>
2023-03-04 21:55:31 +0400

    added previewMdx2

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,16 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-scss",
-    "@storybook/addon-controls",
-    "@storybook/addon-actions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-scss',
+    '@storybook/addon-controls',
+    '@storybook/addon-actions',
   ],
   core: {
-    builder: "webpack5",
+    builder: 'webpack5',
+  },
+  features: {
+    previewMdx2: true,
   },
 };
